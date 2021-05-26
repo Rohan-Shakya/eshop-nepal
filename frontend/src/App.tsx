fe10bd115d021cfc3c67f0542203d32999fa10be
Rohan <rohanshakya254@gmail.com>
2021-05-26 13:51:25 +0545

    initialized react-typescript app and added resources

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <h1>Eshop Nepal</h1>
+      </header>
+    </main>
+  );
+}
+
+export default App;
