aaaac0f0c6dab37a51ba98a63d1cf07875a8bc94
Rohan <rohanshakya254@gmail.com>
2021-05-26 18:05:39 +0545

    serialized Models

--- api/views.py
+++ api/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render
 # from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .products import products
+from .models import Product
+from .serializers import ProductSerializer
 
 
 @api_view(['GET'])
 def getProduts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for item in products:
-        if item['_id'] == pk:
-            product = item
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
