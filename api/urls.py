3999f8888be0cf334db597d63c64f283fc261abc
Rohan <rohanshakya254@gmail.com>
2021-05-27 10:10:45 +0545

    installed JWT and customized

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('products/', views.getProduts, name='products'),
     path('products/<str:pk>/', views.getProduct, name='product')
 ]
