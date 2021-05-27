78c569ef163f1305f9fa442477389d36f6914110
Rohan <rohanshakya254@gmail.com>
2021-05-27 10:54:41 +0545

    User Serializer and Protected Routes

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
+    path('users/profile/', views.getUserProfile, name='users-profile'),
+    path('users/', views.getUsers, name='users'),
     path('products/', views.getProduts, name='products'),
     path('products/<str:pk>/', views.getProduct, name='product')
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 # from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -13,14 +15,32 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
 
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
