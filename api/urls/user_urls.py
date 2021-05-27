59721833a79a667d59714d0592626c156a852715
Rohan <rohanshakya254@gmail.com>
2021-05-27 23:26:10 +0545

    added Update Profile URL

--- api/urls/user_urls.py
+++ api/urls/user_urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
          name='token_obtain_pair'),
     path('register/', views.registerUser, name='register'),
     path('profile/', views.getUserProfile, name='users-profile'),
+    path('profile/update/', views.updateUserProfile, name='users-profile-update'),
     path('', views.getUsers, name='users'),
 ]
