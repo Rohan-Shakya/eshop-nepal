a6004d45e84e5ccc1d393776bf2456484efa20cf
Rohan <rohanshakya254@gmail.com>
2021-05-29 14:35:23 +0545

    added endpoint to Update Order to paid

--- api/views/order_views.py
+++ api/views/order_views.py
@@ -73,3 +73,13 @@ def getOrderById(request, pk):
                             status=status.HTTP_400_BAD_REQUEST)
     except:
         return Response({'detail': 'Order does not exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request, pk):
+    order = Order.objects.get(_id=pk)
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+    return Response('Order was paid')
