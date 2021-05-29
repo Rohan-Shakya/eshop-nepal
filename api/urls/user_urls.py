925ed51eb2117392851db6407d41aa383203a79f
Rohan <rohanshakya254@gmail.com>
2021-05-29 18:22:03 +0545

    added Update User Endpoint

--- api/urls/user_urls.py
+++ api/urls/user_urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('profile/', views.getUserProfile, name='users-profile'),
     path('profile/update/', views.updateUserProfile, name='users-profile-update'),
     path('', views.getUsers, name='users'),
+    path('update/<str:pk>/', views.updateUser, name='user-update'),
     path('delete/<str:pk>/', views.deleteUser, name='user-delete'),
 ]
